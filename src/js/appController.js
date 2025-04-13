179532a5274c84e4a2ca1ddb5b097a10224f1215
Isma <IsmaaTR@users.noreply.github.com>
2025-04-13 18:09:36 +0200

    Added feature: Look for weather info from the server

--- src/js/appController.js
+++ src/js/appController.js
@@ -1,3 +1,27 @@
 import "../styles.css";
+import { weatherModule } from "./weatherModule";
+import { format } from "date-fns";
 
-console.log('working');
+window.appController = (function() {
+
+    const actualDate = format(new Date(), 'yyyy-MM-dd');
+
+    function getActualTemp(city, celsius) {
+        weatherModule.getWeatherInfo(city.toLowerCase(), actualDate, null)
+        .then(weatherInfo => {
+            return weatherInfo.json();
+        })
+        .then(weatherInfoJson => {
+            let temperature = weatherInfoJson.currentConditions.temp;
+            if (celsius) {
+                temperature = (temperature - 32) * (5/9);
+                temperature = Number(temperature.toFixed(1));
+            } 
+            console.log(temperature);
+        }).catch(error => {
+            console.error("An error ocurred during the execution: " + error);
+        });
+    }
+
+    return { getActualTemp }
+})();
