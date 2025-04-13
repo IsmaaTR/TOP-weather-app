b75553b39413e7877ee27055060efa634d62123b
Isma <IsmaaTR@users.noreply.github.com>
2025-04-13 20:02:38 +0200

    Added feature: show weather info

--- src/js/appController.js
+++ src/js/appController.js
@@ -6,21 +6,25 @@ export const appController = (function() {
 
     const actualDate = format(new Date(), 'yyyy-MM-dd');
 
-    function getActualTemp(city, celsius) {
-        weatherModule.getWeatherInfo(city.toLowerCase(), actualDate, null)
-        .then(weatherInfo => {
-            return weatherInfo.json();
-        })
-        .then(weatherInfoJson => {
-            let temperature = weatherInfoJson.currentConditions.temp;
-            if (celsius) {
-                temperature = (temperature - 32) * (5/9);
-                temperature = Number(temperature.toFixed(1));
-            } 
-            console.log(temperature);
-        }).catch(error => {
-            console.error("An error ocurred during the execution: " + error);
-        });
+    async function getActualTemp(city, celsius) {
+        const weatherInfo = await weatherModule.getWeatherInfo(city.toLowerCase(), actualDate, null);
+        const weatherInfoJson = await weatherInfo.json();
+        let temperature = weatherInfoJson.currentConditions.temp;
+        let minTemperature = weatherInfoJson.days[0].tempmin;
+        let maxTemperature = weatherInfoJson.days[0].tempmax;
+        if (celsius) {
+            temperature = fahrenheitToCelsius(temperature);
+            minTemperature = fahrenheitToCelsius(minTemperature);
+            maxTemperature = fahrenheitToCelsius(maxTemperature);
+        }
+        let sky = weatherInfoJson.currentConditions.icon;
+        return { temperature, minTemperature, maxTemperature, sky }
+    }
+
+    function fahrenheitToCelsius(fahrenheit) {
+        let temperature = (fahrenheit - 32) * (5/9);
+        temperature = Number(temperature.toFixed(1));
+        return temperature;
     }
 
     return { getActualTemp }
--- src/js/uiModule.js
+++ src/js/uiModule.js
@@ -3,15 +3,21 @@ import { appController } from "./appController";
 window.UIModule = (function() {
     let useCelsius = true; // Estado inicial: Celsius
 
-    function init(appController) {
-        const form = document.getElementById("search-city-form");
-        const toggleUnitButton = document.getElementById("toggle-unit");
+    const form = document.getElementById("search-city-form");
+    const toggleUnitButton = document.getElementById("toggle-unit");
+    const cityNameParagraph = document.querySelector('#city-name');
+    const temperatureParagraph = document.querySelector('#temperature');
+    const skyParagraph = document.querySelector('#sky');
+    const minMaxTempsParagraph = document.querySelector('#min-max-temps');
+
+    function init() {
 
         // Listener para el formulario
-        form.addEventListener("submit", function(event) {
+        form.addEventListener("submit",async function(event) {
             event.preventDefault(); // Evita que el formulario recargue la página
             const cityName = document.getElementById("cityName").value;
-            appController.getActualTemp(cityName, useCelsius);
+            const info = await appController.getActualTemp(cityName, useCelsius);
+            renderWeatherInfo(cityName, info);
         });
 
         // Listener para alternar entre Celsius y Fahrenheit
@@ -19,6 +25,17 @@ window.UIModule = (function() {
             useCelsius = !useCelsius; // Cambia el estado
             toggleUnitButton.innerHTML = useCelsius ? "<b>C</b> / F" : "C / <b>F</b>";
         });
+    }
+
+    function renderWeatherInfo(city, info) {
+        cityNameParagraph.innerHTML = city;
+        temperatureParagraph.innerHTML = useCelsius ? info.temperature + '°C' : info.temperature + '°F';
+        skyParagraph.innerHTML = info.sky;
+        if (useCelsius) {
+            minMaxTempsParagraph.innerHTML = 'Max. ' + info.maxTemperature + ' °C Min. ' + info.minTemperature + '°C';       
+        } else {
+            minMaxTempsParagraph.innerHTML = 'Max. ' + info.maxTemperature + ' °F Min. ' + info.minTemperature + '°F';       
+        }
     }
 
     return { init };
